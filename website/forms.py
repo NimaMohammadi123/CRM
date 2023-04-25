3941820801305c91cd0c3f2e4e5f2cc34a193e64
nima <nima.mohammadi.021@gmail.com>
2023-04-25 23:50:46 +0430

    registration

---
+++ website/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(max_length=100 , widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(max_length=200 , widget=forms.TextInput(attrs={'class':'form-control'}))
+    
+    class Meta:
+        model=User
+        fields = {'username' , 'first_name' , 'last_name' ,'email' , 'password1' , 'password2'}
+        
+
+
+   
