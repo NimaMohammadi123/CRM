cadc87d5be6877d76b2e26ba241f6748449f0260
nima <nima.mohammadi.021@gmail.com>
2023-05-05 23:25:53 +0430

    final-project

--- website/forms.py
+++ website/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from .models import Record
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control'}))
     first_name = forms.CharField(max_length=100 , widget=forms.TextInput(attrs={'class':'form-control'}))
@@ -13,4 +13,15 @@ class SignUpForm(UserCreationForm):
         
 
 
-   
+class AddRecordForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=100 , widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(max_length=200 , widget=forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.CharField(max_length=200 , widget=forms.TextInput(attrs={'class':'form-control'}))
+    phone = forms.CharField(max_length=200 , widget=forms.TextInput(attrs={'class':'form-control'}))
+    address = forms.CharField(max_length=200 , widget=forms.TextInput(attrs={'class':'form-control'}))
+    city = forms.CharField(max_length=200 , widget=forms.TextInput(attrs={'class':'form-control'}))
+    zipcode = forms.CharField(max_length=200 , widget=forms.TextInput(attrs={'class':'form-control'}))
+    
+    class Meta:
+        model = Record
+        exclude = User ,
